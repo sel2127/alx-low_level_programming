873d4dd4a4a1bf1f0565464dd16b9325f86d41b6
Selina Yeshanew <88796374+seli1212@users.noreply.github.com>
2022-06-08 00:02:43 +0100

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - a function that prints the alphabet, in lowercase
+* Return: Always 0 (Succes)
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
