e1dad2ed123f82c410fc762985f8e4c0cdfa669a
Selina Yeshanew <88796374+seli1212@users.noreply.github.com>
2022-06-09 00:25:36 +0100

    commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
 /**
- * _islower - a function that checks for lowercase character
- * @c: single letter input
- * Return: 1 if int c is lowercase, 0 if otherwise
+ * _islower - check for lowercase character
+ * @c: contain the characters to check
+ * Return: 1 on success or 0 otherwise
  */
+
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (c >= 97 && c <= 122)
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
